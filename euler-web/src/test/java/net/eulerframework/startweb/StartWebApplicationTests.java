60bf71eaeb0d6c041b948f4369dfd697c5b346b5
cFrost <cFrost@eulerproject.io>
2018-02-06 11:08:40 +0800

    spring-boot

---
+++ euler-web/src/test/java/net/eulerframework/startweb/StartWebApplicationTests.java
@@ -0,0 +1,16 @@
+package net.eulerframework.startweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
