172dacb4bc0dc0646fa617ead5b69aa017d77f04
cFrost <cFrost@eulerproject.io>
2018-02-06 22:15:02 +0800

    jpa

---
+++ euler-web/src/main/java/net/eulerframework/startweb/entity/User.java
@@ -0,0 +1,21 @@
+package net.eulerframework.startweb.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SYS_USER")
+public class User {
+
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
